f3753509b33a09787e999b0626e0185a13a405c1
iain <iain@iain.nl>
2022-12-17 17:57:07 -0500

    Day 12, part 2

--- day12/day12.go
+++ day12/day12.go
@@ -27,13 +27,14 @@ func Main() {
 	fmt.Println("Day 12")
 
 	grid := make(Grid)
-	var start, end Point
+	var end Point
+	var starts []Point
 
 	for y, line := range shared.ReadLines("day12/full.input") {
 		for x, char := range strings.Split(line, "") {
 			point := Point{x, y}
-			if char == "S" {
-				start = point
+			if char == "S" || char == "a" {
+				starts = append(starts, point)
 			}
 			if char == "E" {
 				end = point
@@ -42,6 +43,22 @@ func Main() {
 		}
 	}
 
+	var shortest int = 1000000
+
+	for _, start := range starts {
+		steps := countSteps(grid, start, end)
+		if steps == 0 {
+			continue
+		}
+		if shortest > steps {
+			shortest = steps
+		}
+		fmt.Println(steps, "steps from", start)
+	}
+	fmt.Println(shortest)
+}
+
+func countSteps(grid Grid, start, end Point) int {
 	frontier := Queue{Path{point: start}}
 	reached := make(Reached)
 
@@ -66,10 +83,10 @@ func Main() {
 	var path *Path = &winning
 	for path != nil {
 		size++
-		fmt.Println(path.point)
+		// fmt.Println(path.point)
 		path = path.parent
 	}
-	fmt.Println(size - 1)
+	return size - 1
 }
 
 func getNeighbors(current Path, grid Grid) []Path {
@@ -101,7 +118,7 @@ func isAllowed(current Path, to Point, grid Grid) bool {
 
 func charToElevation(char string) Elevation {
 	if char == "S" {
-		return Elevation(-1)
+		return Elevation(0)
 	}
 	if char == "E" {
 		return Elevation(26)
