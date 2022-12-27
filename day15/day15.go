2257f25eedc0c6d03af5e3a5c1ff414c39025516
iain <iain@iain.nl>
2022-12-27 12:28:03 -0500

    Day 15, part 2

--- day15/day15.go
+++ day15/day15.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 )
 
+const input = "day15/full.input"
+const max_x = 4_000_000
+const max_y = 4_000_000
+
 type Point struct {
 	x int
 	y int
@@ -12,51 +16,92 @@ type Point struct {
 
 type Coords map[Point]bool
 
+type Sensor map[Point]Point
+
 type Area struct {
 	beacons Coords
-	sensors Coords
+	sensors Sensor
 	signals Coords
 }
 
 func Main() {
 	fmt.Println("Day 15")
-	targetRow := 2_000_000
-	a := parse("day15/full.input", targetRow)
-	draw(a)
-	count := 0
-	for point := range a.signals {
-		if point.y == targetRow {
-			count++
+	a := parse(input)
+
+	// draw(a)
+
+	for sensor, beacon := range a.sensors {
+		d := distance(sensor, beacon)
+		for i := 0; i <= d; i++ {
+			for _, p := range points(sensor, d+1, i) {
+				// fmt.Println("Checking", p)
+				if a.isHiddenBeacon(p) {
+					panic(fmt.Sprintf("FOUND IT: %v, freq: %d", p, tuningFrequency(p)))
+				}
+			}
 		}
 	}
-	for point := range a.beacons {
-		if point.y == targetRow {
-			count--
-		}
-	}
-	fmt.Println(count)
 }
 
-func parse(filename string, targetRow int) Area {
-	a := Area{beacons: make(Coords), sensors: make(Coords), signals: make(Coords)}
+func tuningFrequency(p Point) int64 {
+	return int64(p.x)*int64(max_x) + int64(p.y)
+}
+
+func points(s Point, d, i int) [4]Point {
+	o := d - i
+
+	var points = [4]Point{
+		(Point{s.x + i, s.y - o}),
+		(Point{s.x + i, s.y + o}),
+		(Point{s.x - i, s.y - o}),
+		(Point{s.x - i, s.y + o}),
+	}
+
+	return points
+}
+
+func (a *Area) isSensor(p Point) bool {
+	_, ok := a.sensors[p]
+	return ok
+}
+func (a *Area) isBeacon(p Point) bool {
+	_, ok := a.beacons[p]
+	return ok
+}
+
+func (a *Area) isHiddenBeacon(p Point) bool {
+	if p.x < 0 || p.y < 0 || p.x > max_x || p.y > max_y {
+		return false
+	}
+	for sensor, beacon := range a.sensors {
+		d1 := distance(sensor, beacon)
+		d2 := distance(sensor, p)
+		if d2 <= d1 {
+			return false
+		}
+	}
+	return true
+}
+
+func parse(filename string) Area {
+	a := Area{beacons: make(Coords), sensors: make(Sensor), signals: make(Coords)}
 	for _, line := range shared.ReadLines(filename) {
 		var sx, sy, bx, by int
 		fmt.Sscanf(line, "Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d", &sx, &sy, &bx, &by)
 		sensor := Point{sx, sy}
 		beacon := Point{bx, by}
-		a.sensors[sensor] = true
+		a.sensors[sensor] = beacon
 		a.beacons[beacon] = true
-		d := distance(sensor, beacon)
-		fmt.Println("Found sensor", sx, sy, "found beacon", bx, by, "distance", d)
+		// d := distance(sensor, beacon)
+		// fmt.Println("Found sensor", sx, sy, "found beacon", bx, by, "distance", d)
 
-		y := targetRow
 		// for y := (sensor.y - d); y <= (sensor.y + d); y++ {
-		for x := (sensor.x - d); x <= (sensor.x + d); x++ {
-			signal := Point{x, y}
-			if distance(sensor, signal) <= d {
-				a.signals[signal] = true
-			}
-		}
+		// 	for x := (sensor.x - d); x <= (sensor.x + d); x++ {
+		// 		signal := Point{x, y}
+		// 		if distance(sensor, signal) <= d {
+		// 			a.signals[signal] = true
+		// 		}
+		// 	}
 		// }
 	}
 	return a
@@ -65,16 +110,19 @@ func parse(filename string, targetRow int) Area {
 func draw(a Area) {
 	fmt.Println("sensors", a.sensors)
 	fmt.Println("beacons", a.beacons)
-	for y := -5; y < 30; y++ {
-		for x := -5; x < 30; x++ {
+	o := Point{14, 11}
+	for y := -10; y < 30; y++ {
+		for x := -10; x < 30; x++ {
 			p := Point{x, y}
 			switch {
-			case a.beacons[p]:
+			case a.isBeacon(p):
 				pr("B", 31)
-			case a.sensors[p]:
+			case a.isSensor(p):
 				pr("S", 32)
 			case a.signals[p]:
-				pr("#", 33)
+				pr("░", 33)
+			case p == o:
+				pr("╳", 34)
 			default:
 				pr(" ", 0)
 			}
--- go.mod
+++ go.mod
@@ -3,6 +3,14 @@ module aoc-2022
 go 1.19
 
 require (
+	github.com/VividCortex/ewma v1.1.1 // indirect
+	github.com/cheggaaa/pb/v3 v3.1.0 // indirect
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mattn/go-runewidth v0.0.12 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/sxyazi/go-collection v0.0.3 // indirect
 	golang.org/x/exp v0.0.0-20221215174704-0915cd710c24 // indirect
+	golang.org/x/sys v0.1.0 // indirect
 )
